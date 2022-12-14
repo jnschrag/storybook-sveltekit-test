77cda017acab01904c3b0cc5aacc3ad5d2c2695c
Jacque Schrag <jnschrag@gmail.com>
2022-12-14 10:49:44 -0500

    Initial commit

---
+++ .storybook/main.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@storybook/addon-interactions',
+		'@storybook/addon-svelte-csf'
+	],
+	framework: '@storybook/sveltekit',
+	features: {
+		storyStoreV7: true
+	},
+	docs: {
+		docsPage: 'automatic'
+	}
+};
